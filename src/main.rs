17f198265cc812bd923461d674215e9825f4a04f
Yufan Lou <2263580+louy2@users.noreply.github.com>
2021-06-10 08:26:11 -0400

    Add ecs code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use planck_ecs::{join, Components, DispatcherBuilder, Entities, IntoSystem, SystemResult, World};
+
+#[derive(Debug, PartialEq)]
+enum Color {
+    Red,
+    Blue,
+    Green,
+}
+
+fn change_color_system(colors: &mut Components<Color>) -> SystemResult {
+    for color in join!(&mut colors) {
+        *color = Color::Blue;
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut world = World::default();
+    let mut dispatcher = DispatcherBuilder::default()
+        .add(change_color_system)
+        .build(&mut world);
+
+    let entity1 = world.get_mut_or_default::<Entities>().create();
+    world
+        .get_mut_or_default::<Components<_>>()
+        .insert(entity1, Color::Red);
+
+    dispatcher.run_seq(&mut world).unwrap();
 }
